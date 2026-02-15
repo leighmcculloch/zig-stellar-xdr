9fb485f4d168e99d264667fc215b634c9c074164
Leigh <351529+leighmcculloch@users.noreply.github.com>
2026-02-16 00:23:03 +1000

    separate test module from library module

--- build.zig
+++ build.zig
@@ -4,21 +4,23 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const module = b.addModule("stellar-xdr", .{
+    _ = b.addModule("stellar-xdr", .{
         .root_source_file = b.path("src/generated.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const test_mod = b.createModule(.{
+        .root_source_file = b.path("src/generated_test.zig"),
         .target = target,
         .optimize = optimize,
     });
 
     const tests = b.addTest(.{
-        .root_source_file = b.path("src/generated.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = test_mod,
     });
     const run_tests = b.addRunArtifact(tests);
 
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_tests.step);
-
-    _ = module;
 }
